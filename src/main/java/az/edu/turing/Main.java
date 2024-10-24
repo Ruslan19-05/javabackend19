d3a898d41f8426065b81ec0252afa48fdde8a1e4
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-24 20:32:10 +0400

    mew

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -5,10 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello,World\n");
-           System.out.println("Hello,World");
-        System.out.printf("\nHello,%s","World");
-    System.out.printf("Hello,World: %5f\n   ",50.0);
+        System.out.print("1\n2\n3\n4\n5\n");
+
+
 
 
     }
