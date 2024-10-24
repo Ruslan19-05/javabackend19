2d2693800c66dd718ed75f4b6536da8b00cea87a
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-24 20:15:58 +0400

    fjgwif

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.print("Hello,World\n");
            System.out.println("Hello,World");
         System.out.printf("\nHello,%s","World");
-    System.out.printf("Hello,World: %3f\n   ",50.0);
+    System.out.printf("Hello,World: %5f\n   ",50.0);
 
 
     }
