d1b8d10eafd012ea3e8b3556b73ea9cd24a4bcfe
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-24 20:09:07 +0400

    salam

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello,World\n");
+           System.out.println("Hello,World");
+        System.out.printf("\nHello,%s","World");
+    System.out.printf("Hello,World: %3f\n   ",50.0);
 
 
     }
